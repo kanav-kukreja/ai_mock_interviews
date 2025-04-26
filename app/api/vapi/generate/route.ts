f7648d2dd43c8da1b72e7a80b98b707c4770eb67
Kanav <kanav@Kanavs-MacBook-Air.local>
2025-04-26 17:07:32 +0530

    Test

--- app/api/vapi/generate/route.ts
+++ app/api/vapi/generate/route.ts
@@ -36,7 +36,7 @@ export async function POST(request: Request) {
       coverImage: getRandomInterviewCover(),
       createdAt: new Date().toISOString(),
     };
-
+    console.log(interview)
     await db.collection("interviews").add(interview);
 
     return Response.json({ success: true }, { status: 200 });
